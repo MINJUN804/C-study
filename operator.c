b16d2ba37141538090d3ddcb4adfeb5d25616a2f
DESKTOP-E9BTH74\rlaal <minjunkim804@gmail.com>
2019-04-05 15:58:04 +0900

    c-study
    산술연산을 이용해 초단위 시간입력후 시간,분,초 출력

---
+++ operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+int main(void)
+{
+	int time;
+	int hour, min, sec;
+	printf("time(sec):");
+	scanf("%d", &time);
+
+	hour = time / 60;
+	min = time / (60^2);
+	sec = time % (60^4);
+
+	printf("hour:%d min:%d sec:%d \n", hour, min, sec);
+	return 0;
+
+
+}
+*/
