c2ba0203f1f34fd3dae7d54294253ed5982c1e50
DESKTOP-E9BTH74\rlaal <minjunkim804@gmail.com>
2019-04-05 16:02:04 +0900

    c-study
    구조체 사용하여 입력정수 위치 바꾸기

---
+++ structure_swap.c
@@ -0,0 +1,35 @@
+/*
+typedef struct point
+{
+	int xpos;
+	int ypos;
+}Point;
+
+void SwapPoint(Point* pos1, Point* pos2 )
+{
+	Point* temp;
+	temp = pos1;
+	pos1 = pos2;
+	pos2 = temp;
+
+	printf("%d %d \n %d %d", pos1->xpos, pos1->ypos, pos2->xpos, pos2->ypos);
+
+	
+}
+
+int main(void)
+{
+	Point pos1 = {2,4};
+	Point pos2 = { 5,7 };
+	printf("%d %d \n %d %d", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+	printf("\n");
+	
+	SwapPoint(&pos1, &pos2);
+
+	//printf("%d %d \n %d %d", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+
+	return 0;
+
+}
+
+*/
