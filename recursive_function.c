9a2723ec2de880db0485dbc9f81154a7f601a609
DESKTOP-E9BTH74\rlaal <minjunkim804@gmail.com>
2019-04-05 16:01:06 +0900

    c-study
    재쉬함수를 이용해 2의 8승을 출력

---
+++ recursive_function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+/*
+int func(int n);
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+
+	printf("2^ n:%d \n", func(n));
+
+	return 0;
+
+}
+
+
+int func(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return 2 * func(n - 1);
+}
+*/
